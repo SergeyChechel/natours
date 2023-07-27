e1654c6bf993c0ca52503c2caffb82f58b0f52f5
sergey <chechelsk@gmail.com>
2023-07-27 10:46:16 +0300

    Prepared app for deployment

--- models/tourModel.js
+++ models/tourModel.js
@@ -168,10 +168,10 @@ tourSchema.pre(/^find/, function (next) {
   next();
 });
 
-tourSchema.post(/^find/, function (docs, next) {
-  console.log(`Query took ${Date.now() - this.start} ms`);
-  next();
-});
+// tourSchema.post(/^find/, function (docs, next) {
+//   console.log(`Query took ${Date.now() - this.start} ms`);
+//   next();
+// });
 
 // AGREGATION MIDDLEWARE
 // tourSchema.pre('aggregate', function (next) {
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 process.on('uncaughtException', (err) => {
-  console.log(err.name, err.message);
+  // console.log(err.name, err.message);
   console.log('UNHANDLED EXCEPTION 💥 Shutting down...');
   process.exit(1);
 });
@@ -32,7 +32,7 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
+  // console.log(err.name, err.message);
   console.log('UNHANDLED REJECTION 💥 Shutting down...');
   server.close(() => process.exit(1));
 });
