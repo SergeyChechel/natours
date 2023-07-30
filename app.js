eb05a77f33d8db2d59852d3845e75adb33827c11
sergey <chechelsk@gmail.com>
2023-07-30 16:47:30 +0300

    Implemented webhook stripe

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const viewRouter = require('./routes/viewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
+const bookingController = require('./controllers/bookingController');
 
 // Start express app
 const app = express();
@@ -58,6 +59,12 @@ const limiter = rateLimit({
   message: 'Too many requests from this IP, please try again in an hour!',
 });
 app.use('/api', limiter);
+
+app.post(
+  '/webhook-checkout',
+  express.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout
+);
 
 // Request BODY PARSER, reding data from body into req.body
 app.use(express.json({ limit: '10Kb' }));
